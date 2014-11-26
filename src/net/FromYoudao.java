30b3efc834d46dea558256d7aedc97a4c8899b5c
Jam-G <cruijunguo@gmail.com>
2014-11-26 20:37:09 +0800

    jam

--- src/net/FromYoudao.java
+++ src/net/FromYoudao.java
@@ -6,6 +6,8 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import word.Word;
 
@@ -19,19 +21,52 @@ public class FromYoudao extends WordEngine{
 		//System.out.print(text_HTML);
 		if(text_HTML == null || text_HTML.length() == 0)
 			return null;//text error 
-		String wo = null;
-		String pron_EN_UK = null;
-		String pron_EN_US = null;
-		ArrayList<String> explain = new ArrayList<String>();
-		
-		
-		Word word = new Word();
-		word.setWord(wo);
-		word.setPron_EN_US(pron_EN_US);
-		word.setPron_EN_UK(pron_EN_UK);
-		word.setExplain(explain);
-		
-		return word;
+		//String wo = null;
+		//String pron_EN_UK = null;
+		//String pron_EN_US = null;
+				
+		Pattern wordPattern = Pattern.compile("(?<=<div id=\"phrsListTab\" class=\"trans-wrapper clearfix\">\\s{29}<h2 class=\"wordbook-js\">)[\u0000-\uFFFF]*(?=</div>\\s*</div>\\s*<div id=\"webTrans\" class=\"trans-wrapper trans-tab\">)");
+		Matcher m = wordPattern.matcher(text_HTML);
+		String wordString = null;
+		Word theWord = new Word();
+		if(m.find()){
+			wordString = m.group();
+			System.out.println(wordString);
+			Pattern word = Pattern.compile("(?<=<span class=\"keyword\">)[\u0000-\uFFFF]*(?=</span>\\s*<div class=\"baav\">)");//word must has word letters or space or ' 
+			Pattern pron_EN_UK = Pattern.compile("(?<=\"head\":\\{\"en\":\\{\"punc\":\")[^\"]*(?=\",\"mp3\":\")");
+			Pattern pron_EN_US = Pattern.compile("(?<=\\},\"am\":\\{\"punc\":\")[^\"]*(?=\",\"mp3\":\")");
+			Pattern explains = Pattern.compile("(?<=phonetic: phonetic,\\s{4}explain: \")[\u0000-\uFFFF]*(?=\")");
+			m = word.matcher(wordString);
+			if(m.find())
+				theWord.setWord(m.group());
+			System.out.println(theWord.getWord());
+			m = pron_EN_UK.matcher(wordString);
+			if(m.find())
+				theWord.setPron_EN_UK(m.group());
+			System.out.println(theWord.getPron_EN_UK());
+			m = pron_EN_US.matcher(wordString);
+			if(m.find())
+				theWord.setPron_EN_US(m.group());
+			System.out.println(theWord.getPron_EN_US());
+			m = explains.matcher(wordString);
+			if(m.find()){
+			//	System.out.println(m.group());
+				String[] ex = m.group().split("<br />");
+				ArrayList<String> explain = new ArrayList<String>();
+				for(String str:ex){
+					System.out.println(str);
+					explain.add(str);
+				}
+				theWord.setExplain(explain);
+			}
+		}else{
+					
+			return null;
+		}
+		//System.out.println( wordString);
+			
+				
+		return theWord;
 	}
 
 	@Override
@@ -39,6 +74,9 @@ public class FromYoudao extends WordEngine{
 		// TODO Auto-generated method stub
 		return new URL(url_prefix + word.replaceAll(" ", "%20") + "&keyfrom=dict.index");
 	}
-	
+	public static void main(String[] args){
+		WordEngine youdao = new FromYoudao();
+		youdao.search("D.a");
+	}
 
 }
